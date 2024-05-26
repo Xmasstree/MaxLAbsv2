f33c8bc72423f11980d95ed72318aeeec9122c1b
Ilya Smolnikov <ilya.sortym@gmail.com>
2024-05-27 00:55:26 +0500

    swagger

---
+++ MaxLabv2/Controllers/WebOutController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.OpenApi.Models;
+using System.IO;
+using Microsoft.AspNetCore.Authentication;
+
+
+namespace MaxLabv2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebOutController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Aboba ([FromQuery] Weboo weboo, HttpContext context)
+        {
+            string fileName = "weboo.json";
+            string jsonString = JsonSerializer.Serialize(weboo);
+            System.IO.File.WriteAllText(fileName, jsonString);
+            byte[] mas = System.IO.File.ReadAllBytes(fileName);
+            return Ok(Results.Json(weboo));
+            //return File(mas, "application/json", fileName) ;
+        }
+    }
+}
---
+++ MaxLabv2/WebOut.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MaxLabv2
+{
+    public class WebOut
+    {
+        public string st1 { get; set; }
+        public Dictionary<char, int> letters { get; set; }
+        public string st2 { get; set; }
+        public string sortST { get; set; }
+        public string rand { get; set; }
+        public string shortst { get; set; }
+    }
+
+    public class Weboo
+    {
+      
+        public string rand { get; set; }
+        public string abob { get; set; }
+    }
+}

cf5212abc4aa9fa7a9af11049889185355960570
Ilya Smolnikov <ilya.sortym@gmail.com>
2024-05-28 03:11:06 +0500

    lab9_done

--- MaxLabv2/Controllers/WebOutController.cs
+++ MaxLabv2/Controllers/WebOutController.cs
@@ -8,6 +8,7 @@ using System.Text.Json.Serialization;
 using Microsoft.OpenApi.Models;
 using System.IO;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
 
 
 namespace MaxLabv2.Controllers
@@ -17,14 +18,25 @@ namespace MaxLabv2.Controllers
     public class WebOutController : ControllerBase
     {
         [HttpGet]
-        public IActionResult Aboba ([FromQuery] Weboo weboo, HttpContext context)
+        public IActionResult Aboba ([FromQuery] string a, [FromQuery] string sw)
         {
-            string fileName = "weboo.json";
-            string jsonString = JsonSerializer.Serialize(weboo);
-            System.IO.File.WriteAllText(fileName, jsonString);
-            byte[] mas = System.IO.File.ReadAllBytes(fileName);
-            return Ok(Results.Json(weboo));
-            //return File(mas, "application/json", fileName) ;
+            WebOut webOut = Programs.Base(a, sw);
+            if (webOut.st1 == "ex1") 
+            {
+                return BadRequest($"Inwalid parametrs: {webOut.st2}");
+            }
+            if (webOut.st1 == "ex2")
+            {
+                return BadRequest("Inwalid parametrs");
+            }
+            else
+            {
+                string fileName = "weboo.json";
+                string jsonString = JsonSerializer.Serialize(webOut);
+                System.IO.File.WriteAllText(fileName, jsonString);
+                byte[] mas = System.IO.File.ReadAllBytes(fileName);
+                return File(mas, "application/json", fileName);
+            }
         }
     }
 }
